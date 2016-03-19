8f0f171c44db7380a6a06c64fead3c37bbce1326
Anonymous Candidate <anon@example.com>
2016-03-19 14:55:57 -0700

    (Enable cider nREPL)

--- project.clj
+++ project.clj
@@ -4,8 +4,11 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
+                 [refactor-nrepl "1.1.0"]
                  [ring/ring-defaults "0.1.5"]]
-  :plugins [[lein-ring "0.9.7"]]
+  :plugins [[cider/cider-nrepl "0.9.1"]
+            [refactor-nrepl "1.1.0"]
+            [lein-ring "0.9.7"]]
   :ring {:handler todos.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
