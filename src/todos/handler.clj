f49a8f5bc0d83ae1770de5560de56bb2bff21a19
Anonymous Candidate <anon@example.com>
2016-03-19 15:43:48 -0700

    Connected to MySQL and created the todo table

--- src/todos/handler.clj
+++ src/todos/handler.clj
@@ -1,13 +1,17 @@
 (ns todos.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [todos.business :as business]
+            [todos.data.db :refer [initial-db-setup]]))
 
 (defroutes app-routes
   (GET "/" [] (slurp  "resources/public/under-construction.html"))
   (GET "/about" [] (slurp "resources/public/about.html"))
   (route/resources "/")
   (route/not-found "Not Found"))
+
+(defonce db-setup (initial-db-setup business/db-settings))
 
 (def app
   (wrap-defaults app-routes site-defaults))
