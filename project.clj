d451033e51d17e5d4d9561089381ae189528f958
Anonymous Candidate <anon@example.com>
2016-03-19 14:57:46 -0700

    (Updated project.clj)
    1. compojure 1.5.0
    2. cider-nrepl 0.9.1
    3. java.jdbc and mysql driver for MySQL

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.4.0"]
+                 [cider/cider-nrepl "0.9.1"]
+                 [compojure "1.5.0"]
+                 [mysql/mysql-connector-java "5.1.8"]
+                 [org.clojure/java.jdbc "0.4.2"]
                  [refactor-nrepl "1.1.0"]
                  [ring/ring-defaults "0.1.5"]]
   :plugins [[cider/cider-nrepl "0.9.1"]
