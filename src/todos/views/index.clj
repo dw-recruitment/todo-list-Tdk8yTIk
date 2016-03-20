875fbee7d14ea65bb7b3926975a73eb8de9847b7
Anonymous Candidate <anon@example.com>
2016-03-19 23:43:49 -0700

    Added the form to enter a new TODO

--- src/todos/views/index.clj
+++ src/todos/views/index.clj
@@ -1,5 +1,7 @@
 (ns todos.views.index
   (require [hiccup.core :refer :all]
+           [hiccup.form :as form]
+           [ring.util.anti-forgery :as anti-forgery]
            [todos.views.page :refer [page]]))
 
 (defn- todo-item
@@ -15,11 +17,21 @@
   (page
    "TODO List"
 
-   [:div#todolist
+   [:div
+    ;; TODO List
+    [:div#todolist
      [:h1 "TODO List"]
      [:table nil
       [:thead nil
        [:tr nil '([:th nil "ID"] [:th nil "TODO"] [:th nil "Done?"])]]
       [:tbody nil
        (for [row data]
-         [:tr nil (todo-item row)])]]]))
+         [:tr nil (todo-item row)])]]]
+
+    ;; FORM
+    [:div#form
+     (form/form-to [:post "/"]
+                   ;; anti-forgery token needed for POST
+                   (anti-forgery/anti-forgery-field)
+                   (form/text-field "todo")
+                   (form/submit-button "Add TODO"))]]))
