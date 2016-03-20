483a4ba9083333f44ef61aa71de154d7d5137eec
Anonymous Candidate <anon@example.com>
2016-03-20 00:18:56 -0700

    Completed Step 6
    Added a few tests for GET routes.
    TODO: Add tests for POST.

--- test/todos/handler_test.clj
+++ test/todos/handler_test.clj
@@ -1,14 +1,29 @@
 (ns todos.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [todos.handler :refer :all]))
+            [todos.data.db :as db]
+            [todos.handler :refer :all]
+            [todos.views.about :as about]
+            [todos.views.index :as index]))
 
 (deftest test-app
   (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+    (let [{:keys [body status]} (app (mock/request :get "/"))]
+      (is (= status 200))
+      ;; anti-forgery token part is unbound in (-> (db/get-todolist) (index/contents))
+      ;; while there is a valid token in mock request
+      ;(is (= body (-> (db/get-todolist) (index/contents))))
+      ))
+
+  ;; TODO: Add a test for POST "/"
+  ;;       Learn about anti-forgery / POST handling.
+
+  (testing "about route"
+    (let [{:keys [body status]} (app (mock/request :get "/about"))]
+      (is (= status 200))
+      (is (= body (about/contents)))))
 
   (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+    (let [{:keys [body status]} (app (mock/request :get "/invalid"))]
+      (is (= status 404))
+      (is (= body "Not Found")))))
